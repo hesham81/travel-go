775f4e126c50def3dd3d7919e3c106e3de2d2935
hesham81 <107004690+hesham81@users.noreply.github.com>
2025-03-07 05:32:34 +0200

    add search to browse_trip_departure.dart

--- lib/modules/layout/pages/admin/pages/trip_departures/pages/browse_departure/pages/browse_trip_departure.dart
+++ lib/modules/layout/pages/admin/pages/trip_departures/pages/browse_departure/pages/browse_trip_departure.dart
@@ -1,19 +1,41 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:travel_go/core/routes/route_transact.dart';
+import 'package:travel_go/models/trip_data_model.dart';
 import 'package:travel_go/modules/layout/pages/admin/pages/trip_departures/pages/browse_departure/pages/explore_selected_list_of_departures.dart';
 import '/core/providers/collections_provider.dart';
 import '/core/extensions/extensions.dart';
 import '/core/theme/app_colors.dart';
 import '/modules/layout/pages/admin/pages/trip_departures/pages/browse_departure/widget/trip_explorer.dart';
 
-class BrowseTripDeparture extends StatelessWidget {
+class BrowseTripDeparture extends StatefulWidget {
   const BrowseTripDeparture({super.key});
+
+  @override
+  State<BrowseTripDeparture> createState() => _BrowseTripDepartureState();
+}
+
+class _BrowseTripDepartureState extends State<BrowseTripDeparture> {
+  final TextEditingController searchController = TextEditingController();
+  List<TripDataModel> searchList = [];
 
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context).textTheme;
     var provider = Provider.of<CollectionsProvider>(context);
+    onSearchChanged(String searchQuery) {
+      searchList = [];
+      for (var trip in provider.getAllTripsData) {
+        if (trip.tripName.toLowerCase().contains(
+              searchQuery.toLowerCase(),
+            )) {
+          searchList.add(trip);
+        }
+      }
+      setState(() {});
+    }
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -34,6 +56,14 @@ class BrowseTripDeparture extends StatelessWidget {
         child: Column(
           children: [
             0.01.height.hSpace,
+            CupertinoSearchTextField(
+              controller: searchController,
+              onChanged: (value) {
+                onSearchChanged(value);
+                setState(() {});
+              },
+            ),
+            0.01.height.hSpace,
             ListView.separated(
               shrinkWrap: true,
               physics: NeverScrollableScrollPhysics(),
@@ -42,16 +72,22 @@ class BrowseTripDeparture extends StatelessWidget {
                   context,
                   SlideRightRoute(
                     page: ExploreSelectedListOfDepartures(
-                      model: provider.getAllTripsData[index],
+                      model: (searchList.isNotEmpty)
+                          ? searchList[index]
+                          : provider.getAllTripsData[index],
                     ),
                   ),
                 ),
                 child: TripExplorer(
-                  model: provider.getAllTripsData[index],
+                  model: (searchList.isNotEmpty)
+                      ? searchList[index]
+                      : provider.getAllTripsData[index],
                 ),
               ),
               separatorBuilder: (context, _) => 0.01.height.hSpace,
-              itemCount: provider.getAllTripsData.length,
+              itemCount: searchList.isNotEmpty
+                  ? searchList.length
+                  : provider.getAllTripsData.length,
             ),
           ],
         ).hPadding(0.03.width),
